8de597f0d106e8e51e98375dec39eca5d824949c
narpheus <137093400+narpheus@users.noreply.github.com>
2025-07-20 01:15:11 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('.'));
+
+let players = [];
+let hands = {};
+let lives = 3;
+let shuriken = 1;
+let lastPlayed = 0;
+
+io.on('connection', (socket) => {
+  console.log('접속됨:', socket.id);
+  players.push(socket.id);
+
+  socket.on('disconnect', () => {
+    players = players.filter(id => id !== socket.id);
+    delete hands[socket.id];
+    console.log('퇴장:', socket.id);
+  });
+
+  socket.on('start', () => {
+    const deck = Array.from({ length: 100 }, (_, i) => i + 1);
+    shuffle(deck);
+
+    lives = 3;
+    shuriken = 1;
+    lastPlayed = 0;
+
+    hands = {};
+    players.forEach(id => {
+      hands[id] = [deck.pop()];
+      io.to(id).emit('hand', hands[id]);
+    });
+
+    io.emit('update-resources', { lives, shuriken });
+  });
+
+  socket.on('play', (card) => {
+    if (card < lastPlayed) {
+      lives -= 1;
+      io.emit('life-lost');
+    } else {
+      lastPlayed = card;
+    }
+
+    io.emit('played', { by: socket.id.slice(0, 5), card });
+    io.emit('update-resources', { lives, shuriken });
+  });
+
+  socket.on('use-shuriken', () => {
+    if (shuriken <= 0) return;
+    shuriken -= 1;
+
+    let minCards = [];
+    players.forEach(id => {
+      if (hands[id] && hands[id].length > 0) {
+        minCards.push({ id, card: Math.min(...hands[id]) });
+      }
+    });
+
+    const globalMin = Math.min(...minCards.map(x => x.card));
+
+    // 공개하고 손에서 제거
+    for (let obj of minCards) {
+      if (obj.card === globalMin) {
+        hands[obj.id] = hands[obj.id].filter(c => c !== obj.card);
+      }
+    }
+
+    io.emit('shuriken-used', globalMin);
+    io.emit('update-resources', { lives, shuriken });
+  });
+});
+
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
+
+http.listen(3000, () => {
+  console.log('서버 실행 중: http://localhost:3000');
+});
